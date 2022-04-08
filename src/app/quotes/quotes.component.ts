bbf3c7959ed33f618c984f554492ab4a7dff85bb
maureen-james <manjax97@gmail.com>
2022-04-08 22:55:43 +0300

    add an array of quote function

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes: Quote[]=[
+    new Quote(1,"first quote",0,0),
+    new Quote(1,"second quote",0,0),
+    new Quote(1,"third quote",0,0),
+  ]
   constructor() { }
 
   ngOnInit(): void {
