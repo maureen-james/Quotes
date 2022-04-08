3dab580c8e8a7004b1a7cb80e166e057ba8823c7
maureen-james <manjax97@gmail.com>
2022-04-09 02:12:09 +0300

    add a highlight directive

--- src/app/high.directive.ts
+++ src/app/high.directive.ts
@@ -5,6 +5,9 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighDirective {
 
-  constructor(private elem:ElementRef) { }
+  constructor(private elem: ElementRef) { 
+    elem.nativeElement.style.backgroundColor='yellow'
+  }
+
 
 }
