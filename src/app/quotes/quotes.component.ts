fc06513de2ec0ae840a16fd914168b5197bc06c8
maureen-james <manjax97@gmail.com>
2022-04-09 05:41:06 +0300

    modify author's name

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,9 +8,9 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
   quotes: Quote[]=[
-    new Quote(1,"Write it on your heart that every day is the best day in the year.","Ralph Waldo Emerson",0,0),
-    new Quote(2,"Accentuate the positive, Eliminate the Negative, latch onto the affirmative.","Bing Crosby",0,0),
-    new Quote(3,"You cannot have a positive life and a negative mind.","Joyce Meyer",0,0)
+    new Quote(1,"Write it on your heart that every day is the best day in the year.","By: Ralph Waldo Emerson",0,0),
+    new Quote(2,"Accentuate the positive, Eliminate the Negative, latch onto the affirmative.","By: Bing Crosby",0,0),
+    new Quote(3,"You cannot have a positive life and a negative mind.","By: Joyce Meyer",0,0)
   ]
   arr:number[]=this.quotes.map(quote=>quote.upvotes)
   highest = Math.max(...this.arr)
