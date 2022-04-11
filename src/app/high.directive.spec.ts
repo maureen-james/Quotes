44a4d050c19454b9ad53facc20650f44033d9ecc
maureen-james <manjax97@gmail.com>
2022-04-12 01:50:17 +0300

    Auto-generated commit [ci skip]

--- src/app/high.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighDirective } from './high.directive';
-
-describe('HighDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighDirective();
-    expect(directive).toBeTruthy();
-  });
-});
